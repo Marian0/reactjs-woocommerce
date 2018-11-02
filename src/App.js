5f22f303bfeb3f29ca37111c143baea69dd9163e
marian0 <marianosantafe@gmail.com>
2018-11-02 15:43:07 -0300

    Starting from the scratch with ant

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-import Layout from './component/Layout/Layout';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <Layout />
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about/">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users/">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Route path="/" exact component={Index}/>
+                    <Route path="/about/" component={About}/>
+                    <Route path="/users/" component={Users}/>
+                </div>
             </Router>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
